7a13cd94adde495251786f249e045b9c02d14852
ngan-digiex <ngan.pham@digiex.group>
2023-02-07 16:08:08 +0700

    UI chat title & chat text

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,19 +1,55 @@
 import React from 'react';
-import {FiLogOut} from 'react-icons/fi';
-import {BsTrash} from 'react-icons/bs';
-import {BsFillLightbulbOffFill} from 'react-icons/bs';
+import { FiLogOut } from 'react-icons/fi';
+import { BsTrash } from 'react-icons/bs';
+import { BsFillLightbulbOffFill } from 'react-icons/bs';
 
 export default function Sidebar() {
+  const buttonClassNames = `
+    text-white 
+    text-xs
+
+    w-full
+    
+    focus:outline-none 
+    focus:ring-4 
+    focus:ring-gray-300 
+    font-medium rounded-lg 
+    
+    py-2
+    
+    bg-gray-800 
+    ark:bg-gray-800 
+    dark:hover:bg-gray-700 
+    dark:focus:ring-gray-700
+    hover:bg-gray-900 
+  `
   return (
     <>
-      <div className='content-between grid p-3 bg-gray-900 font-white w-2/12 h-screen text-white-900'>
+      <div className='content-between grid p-3 bg-primary font-white h-full text-white-900 rounded-l-xl rounded-r-lg'>
         <div>
-          <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 pr-32 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">+ New Chat</button>
+          <button type="button" className={`${buttonClassNames} dark:border-gray-700`}>+ New Chat</button>
         </div>
-        <div className='grid content-end border-t-2 border-indigo-500 pt-2'>
-          <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-3.5 pr-30 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 flex leading-3"><span className='pr-2'><BsTrash /></span> Clear conversations</button>
-          <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-3.5 pr-30 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 flex leading-3"><span className='pr-2'><BsFillLightbulbOffFill /></span> Light mode</button>
-          <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-3.5 pr-32 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 flex leading-3"><span className='pr-2'><FiLogOut /></span> Log out</button>
+        <div className='flex flex-col gap-2 border-t-2 border-indigo-500 pt-2'>
+          <button type="button" className={`${buttonClassNames} dark:border-gray-700 flex leading-3`}>
+            <span className={`mx-3`}>
+              <BsTrash />
+            </span>
+            Clear conversations
+          </button>
+
+          <button type="button" className={`${buttonClassNames} dark:border-gray-700 flex leading-3`}>
+            <span className={`mx-3`}>
+              <BsFillLightbulbOffFill />
+            </span>
+            Light mode
+          </button>
+
+          <button type="button" className={`${buttonClassNames} dark:border-gray-700 flex leading-3`}>
+            <span className={`mx-3`}>
+              <FiLogOut />
+            </span>
+            Log out
+          </button>
         </div>
       </div>
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,35 @@
 
 import Head from 'next/head';
-import Sidebar from '@/components/Sidebar';
-import Rightbar from '@/components/Rightbar';
-import ChatBody from '@/components/ChatBody/ChatBody';
+import MyImg from '@/public/images/myImg.jpg'
+
+import ChatSpace, { ChatProps, UserInfo } from '@/components/Chat/ChatSpace';
 
 
 export default function Home() {
+  const info: UserInfo = {
+    image: {
+      width: 30,
+      height: 30,
+      url: MyImg
+    },
+    fullName: "Hong Tang",
+    id: "1"
+  }
   return (
-    <div className='flex w-full'>
-      <Sidebar/>
-      <ChatBody/>
-    </div>
+    <>
+      <Head>
+        <title>Chat GPT</title>
+        <meta
+          name="description"
+          content="Chat GPT"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={`container p-3 h-[100vh] w-[800px] mx-auto`}>
+        <ChatSpace info={info}/>
+      </div>
+    </>
+    
   );
 }
