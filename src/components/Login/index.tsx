4eeda2b589c19611ca7fe898ef39e4866749f7be
Hong Tang | Tester <77006112+hongtang-digiex@users.noreply.github.com>
2023-02-08 15:40:11 +0700

    met qua huhu

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,96 @@
+import Input from '@/utils/components/Input';
+import Image from 'next/image';
+import React, { useState } from 'react';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+import GoogleLoginButton from '@/utils/components/GoogleButton';
+
+import ChatGPTLogo from '@/public/images/ChatGPTLogo.png';
+
+export default function index() {
+    const [isDisplayPassword, setDisplayPassword] = useState(false);
+    const handleChangeDisplayPassword = () => {
+        setDisplayPassword((prev) => !prev);
+    };
+
+    const eyeButtonClassNames = `bg-primary py-2 px-3 rounded-lg`;
+    return (
+        <div className="flex flex-col justify-center h-full gap-3 mx-auto border max-w-[400px] bg-secondary">
+            <div className={`text-center flex flex-col gap-3`}>
+                <h1
+                    className={`font-bold text-2xl flex items-center justify-center gap-2`}
+                >
+                    <Image
+                        className="rounded-full"
+                        src={ChatGPTLogo}
+                        alt="Chat GPT icon"
+                        width={48}
+                        height={48}
+                    />
+                    Chat GPT
+                </h1>
+                <div className="px-6 text-sm">
+                    <h2>Welcome to the coolest chatGPT experience ever!</h2>
+                    <h2>
+                        Here, we have the latest security features to keep your information
+                        safe and secure, so you can relax and enjoy your time on the site.
+                        Thanks!
+                    </h2>
+                </div>
+            </div>
+            <div className={`login-inputs flex flex-col items-center`}>
+                <div className={`mb-3 w-3/4`}>
+                    <Input
+                        background="bg-secondary border rounded-lg text-xs p-2"
+                        button={{
+                            isContained: false,
+                            element: undefined,
+                            background: undefined,
+                        }}
+                        type="text"
+                        placeholderText="Email@"
+                    />
+                </div>
+                <div className={`mb-3 w-3/4`}>
+                    <Input
+                        background="bg-secondary border rounded-lg text-xs p-2"
+                        button={{
+                            isContained: true,
+                            background: 'bg-primary',
+                            element: isDisplayPassword ? (
+                                <button
+                                    className={eyeButtonClassNames}
+                                    onClick={handleChangeDisplayPassword}
+                                >
+                                    <AiFillEyeInvisible />
+                                </button>
+                            ) : (
+                                <button
+                                    className={eyeButtonClassNames}
+                                    onClick={handleChangeDisplayPassword}
+                                >
+                                    <AiFillEye />
+                                </button>
+                            ),
+                        }}
+                        type={isDisplayPassword ? 'text' : 'password'}
+                        placeholderText="Password"
+                    />
+                </div>
+                <div className={`mb-3 w-3/4 `}>
+                    <button
+                        className={`h-[42px] bg-primary w-full text-secondary font-extrabold hover:bg-blue-400 hover:shadow-md rounded-sm`}
+                        onClick={handleChangeDisplayPassword}
+                    >
+                        Sign in
+                    </button>
+                </div>
+
+                <hr className={`mb-3 w-3/4 text-center gap-3 flex flex-col`} />
+
+                <div className={`w-3/4`}>
+                    <GoogleLoginButton />
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,8 +25,11 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={""} />
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@200&family=Orbitron:wght@500&family=Saira:ital,wght@1,500&display=swap" rel="stylesheet" />
       </Head>
-      <div className={`container p-3 h-[100vh] w-full mx-auto`}>
+      <div className={`py-3 w-full mx-auto h-[92vh] mt-3 container`}>
         <ChatSpace info={info} />
       </div>
     </>
