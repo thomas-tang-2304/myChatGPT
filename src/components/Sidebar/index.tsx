3c9272053cc3736d066c7e1ebb1cfca9e28c2475
quangtrinh-digiex <trinh.pham@digiex.group>
2023-02-09 14:26:08 +0700

    rollback commit

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,7 +5,6 @@ import { BsFillLightbulbOffFill } from 'react-icons/bs';
 import { BsFillLightbulbFill } from 'react-icons/bs';
 import { BsFillChatRightFill } from 'react-icons/bs';
 import { TiDeleteOutline } from 'react-icons/ti';
-<<<<<<< Updated upstream
 import ChatSpace from '../Chat/ChatSpace';
 import styles from '@/styles/Home.module.css';
 
@@ -13,8 +12,6 @@ const APP_THEMES = {
   DARK: 'DARK',
   LIGHT: 'LIGHT',
 }
-=======
->>>>>>> Stashed changes
 
 const newChatClassNames = `
   text-white 
@@ -42,7 +39,6 @@ const newChatClassNames = `
   hover:bg-gray-900 
 `
 
-<<<<<<< Updated upstream
 interface SidebarProps {
   containerClass?: string
 }
@@ -52,53 +48,28 @@ export default function Sidebar({ containerClass }: SidebarProps) {
   const containerClassNames = containerClass ? containerClass : `content-between grid p-3 bg-primary font-white h-full text-white-900 rounded-l-xl rounded-r-lg`
 
   const buttonClassNames = `
-=======
-const buttonClassNames = `
->>>>>>> Stashed changes
     text-white 
     text-lg
     flex
     items-center
     justify-center
-<<<<<<< Updated upstream
 
     w-full
     
-=======
-    w-full
-  
->>>>>>> Stashed changes
     focus:outline-none 
     focus:ring-4 
     focus:ring-gray-300 
     font-medium rounded-lg 
-<<<<<<< Updated upstream
     
     py-2
     
-=======
-  
-    py-2
-  
->>>>>>> Stashed changes
     bg-gray-800 
     ark:bg-gray-800 
     dark:hover:bg-gray-700 
     dark:focus:ring-gray-700
     hover:bg-gray-900 
-<<<<<<< Updated upstream
   `
 
-=======
-`
-
-export default function Sidebar({ theme, setTheme }: any) {
-  const [listIntent, setListIntent] = useState<any>([]);
-  const [hoverListIntent, setHoverListIntent] = useState(false);
-
-  const handleRemoveChat = (id: any) => {
-    const newList = listIntent.filter((item: any) => item.id !== id);
->>>>>>> Stashed changes
 
   const dark = ` 
   text-white-900
@@ -121,11 +92,7 @@ export default function Sidebar({ theme, setTheme }: any) {
 
 
   const addNewChat = () => {
-<<<<<<< Updated upstream
     const newChat = chat.concat({ name: `Bạn tên gì? ${chat.length}`, id: chat.length });
-=======
-    const newChat = listIntent.concat({ name: `Bạn tên gì? ${listIntent.length}`, id: listIntent.length });
->>>>>>> Stashed changes
 
     setChat(newChat);
   };
@@ -141,7 +108,6 @@ export default function Sidebar({ theme, setTheme }: any) {
         {/* <div className={`${containerClassNames}`}> */}
         <div>
           <div className='p-2'><button onClick={addNewChat} type="button" className={`${buttonClassNames} mb-2 dark:border-gray-700`}>+ New Chat</button></div>
-<<<<<<< Updated upstream
           {chat.map((item: any) =>
             <a href='#' key={item.id} className={`${newChatClassNames}`}>
               <span><BsFillChatRightFill /></span>
@@ -149,17 +115,6 @@ export default function Sidebar({ theme, setTheme }: any) {
               <button onClick={() => handleRemoveChat(item.id)}><TiDeleteOutline /></button>
             </a>
           )}
-=======
-          <div className={`${hoverListIntent ? 'overflow-y-scroll' : 'overflow-hidden'} h-[300px]`} onMouseEnter={() => setHoverListIntent(true)} onMouseLeave={() => setHoverListIntent(false)}>
-            {listIntent.map((item: any) =>
-              <a href='#' key={item.id} className={`${newChatClassNames}`}>
-                <span><BsFillChatRightFill /></span>
-                {item.name}
-                <button onClick={() => handleRemoveChat(item.id)}><TiDeleteOutline /></button>
-              </a>
-            )}
-          </div>
->>>>>>> Stashed changes
         </div>
 
         <div className='flex p-3 flex-col gap-2 border-t-2 border-indigo-500 mt-2 pt-2'>
@@ -171,20 +126,11 @@ export default function Sidebar({ theme, setTheme }: any) {
           </button>
 
           <button type="button" className={`${buttonClassNames} dark:border-gray-700 flex leading-3`} onClick={toggleTheme} >
-<<<<<<< Updated upstream
             {theme === APP_THEMES.LIGHT
               ?
               (<><span className={`mx-3`}><BsFillLightbulbFill /></span><span>Light Mode</span></>)
               :
               (<><span className={`mx-3`}><BsFillLightbulbOffFill /></span><span>Dark Mode</span></>)
-=======
-            {theme === "dark"
-              ?
-              (<><span className={`mx-3 back-secondary`}><BsFillLightbulbFill /></span><span>Light Mode</span></>)
-              :
-              (<><span className={`mx-3 back-secondary`}><BsFillLightbulbOffFill /></span><span>Dark Mode</span></>)
-
->>>>>>> Stashed changes
             }
           </button>
 
