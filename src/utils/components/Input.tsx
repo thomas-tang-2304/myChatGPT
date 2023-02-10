ea4b82b441c66d26de872931f1274c14f63ad944
Thanh Pham | Intern <119922242+hongthanh-digiex@users.noreply.github.com>
2023-02-10 09:30:17 +0700

    Update handle Render message

--- src/utils/components/Input.tsx
+++ src/utils/components/Input.tsx
@@ -18,7 +18,7 @@ export interface InputProps {
 const Input = ({ background, button, placeholderText,type,handleChange,value,onKeyDown}: InputProps) => {
     return (
         <div className={`${background} text-white flex w-full justify-between p-3 rounded-br-lg items-center`}>
-            <input type={type} value={value} className={`focus:ring-0 border-none w-full ${background} text-[#C1C1C1]`} placeholder={placeholderText} 
+            <input type={type} value={value} className={`focus:ring-0 border-none w-full ${background} text-black`} placeholder={placeholderText} 
             onChange={handleChange}
             onKeyDown ={onKeyDown}
             />
