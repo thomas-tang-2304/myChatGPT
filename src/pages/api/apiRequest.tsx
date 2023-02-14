8bf78f79e14978fa8a5ed2bafe7c1c1106cd11ad
Hong Tang | Tester <77006112+hongtang-digiex@users.noreply.github.com>
2023-02-14 17:13:07 +0700

    dung bao nua

--- src/pages/api/apiRequest.tsx
+++ src/pages/api/apiRequest.tsx
@@ -3,21 +3,20 @@ import { Configuration, OpenAIApi } from "openai";
 export const getMessageReponse = async (messagecontent: any): Promise<any> => {
   const configuration = new Configuration({
     apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-  });
 
+  });
   const openai = new OpenAIApi(configuration);
 
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `${messagecontent}`,
-    temperature: 0.5,
-    max_tokens: 100,
+    prompt: `Human ${messagecontent}`,
+    temperature: 0.9,
+    max_tokens: 150,
     top_p: 1,
-    frequency_penalty: 0.5,
-    presence_penalty: 0.5,
-    n: 5,
-  })
-
+    frequency_penalty: 0,
+    presence_penalty: 0.6,
+    stop: [" Human:", " AI:"],
+  });
   return response;
 };
 
