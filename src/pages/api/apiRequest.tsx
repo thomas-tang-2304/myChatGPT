af222c4668fa2d73204216d3c4a54f11774f5834
quangtrinh-digiex <trinh.pham@digiex.group>
2023-02-15 10:01:06 +0700

    .

--- src/pages/api/apiRequest.tsx
+++ src/pages/api/apiRequest.tsx
@@ -1,22 +1,40 @@
 import { Configuration, OpenAIApi } from "openai";
+export let finishedText = '';
+
+
+export const refreshText = () => {
+  finishedText = '';
+};
 
 export const getMessageReponse = async (messagecontent: any): Promise<any> => {
   const configuration = new Configuration({
-    apiKey:'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',
-    
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   });
 
-const openai = new OpenAIApi(configuration);
+  const openai = new OpenAIApi(configuration);
 
-const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: `${messagecontent}`,
-  temperature: 0.6,
-  max_tokens: 1000,
-  top_p: 1,
-  frequency_penalty: 0,
-  presence_penalty: 0,
-})
-return response;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${messagecontent}`,
+    temperature: 0.9,
+    max_tokens: 2048,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0.6,
+    stop: [' Human:', ' AI:'],
+  })
+
+  if (response?.data?.choices[0]?.finish_reason === 'stop')
+    finishedText += response?.data?.choices[0]?.text;
+  else {
+    if (response?.data?.choices[0]?.finish_reason === 'length') {
+      finishedText += response?.data?.choices[0]?.text;
+      await getMessageReponse(messagecontent + response?.data?.choices[0]?.text);
+    }
+  }
+
+  // return finishedText
+
+  return response
 };
 
