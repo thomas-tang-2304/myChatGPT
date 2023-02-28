d4b5aa1f7c28c223135995ec42952b444a24388b
Hong Tang | Tester <77006112+hongtang-digiex@users.noreply.github.com>
2023-02-28 14:23:14 +0700

    fixed

--- src/components/MessageSkeleton/MessageSkeleton.tsx
+++ src/components/MessageSkeleton/MessageSkeleton.tsx
@@ -4,8 +4,8 @@ import ReactLoading from "react-loading";
 import { Message } from '../Message/Message'
 
 
-function MessageSkeleton({ }: Props) {
-  const { theme } = useContext(MessageContext)
+function MessageSkeleton() {
+  const { theme } = useContext<any>(MessageContext)
   return (
     <Message variant='bot' children={<ReactLoading type={'bubbles'} color={`${theme == "light" ? "black" : "white"}`} height='32px' width='32px' />} />
   )
