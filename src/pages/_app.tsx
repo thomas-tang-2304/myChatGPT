b87d7bd3d714fb71f196f2e83eb08322da34291c
Hong Tang | Tester <77006112+hongtang-digiex@users.noreply.github.com>
2023-02-28 13:42:38 +0700

    fix bugs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <MessageProvider>
 
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </MessageProvider>
 
   )
